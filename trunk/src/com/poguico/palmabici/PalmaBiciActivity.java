db337438c29eb083cbda891db640301d69256e87
Sergio <kintaro@aeris.localdomain>
2012-03-05 19:18:31 +0100

    First PalmaBici commit... yummi\!

---
+++ trunk/src/com/poguico/palmabici/PalmaBiciActivity.java
@@ -0,0 +1,13 @@
+package com.poguico.palmabici;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PalmaBiciActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
