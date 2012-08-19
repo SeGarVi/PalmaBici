cb770c6a6dbb3ac30dae4642adbbd6faa1f916b2
Sergio <kintaro@localhost.localdomain>
2012-08-19 20:09:18 +0200

    Changed some icons

--- src/com/poguico/palmabici/ActionBarHelperBase.java
+++ src/com/poguico/palmabici/ActionBarHelperBase.java
@@ -95,7 +95,7 @@ public class ActionBarHelperBase extends ActionBarHelper {
         SimpleMenu tempMenu = new SimpleMenu(mActivity);
         SimpleMenuItem homeItem = new SimpleMenuItem(
                 tempMenu, android.R.id.home, 0, mActivity.getString(R.string.app_name));
-        homeItem.setIcon(R.drawable.ic_home);
+        homeItem.setIcon(R.drawable.home);
         addActionItemCompatFromMenuItem(homeItem);
 
         // Add title text
