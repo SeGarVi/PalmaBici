6caf562aab9f0bed6b273ec53b31fdd472fa707a
Sergio <kintaro@localhost.localdomain>
2012-08-19 18:56:36 +0200

    Added copyright notices.

--- trunk/src/com/poguico/palmabici/SynchronizableActivity.java
+++ trunk/src/com/poguico/palmabici/SynchronizableActivity.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2012 Sergio Garcia Villalonga (yayalose@gmail.com)
+ *
+ * This file is part of PalmaBici.
+ *
+ *    PalmaBici is free software: you can redistribute it and/or modify
+ *    it under the terms of the Affero GNU General Public License version 3
+ *    as published by the Free Software Foundation.
+ *
+ *    PalmaBici is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    Affero GNU General Public License for more details
+ *    (https://www.gnu.org/licenses/agpl-3.0.html).
+ *    
+ */
+
 package com.poguico.palmabici;
 
 import android.app.Activity;
--- trunk/src/com/poguico/palmabici/Synchronizer.java
+++ trunk/src/com/poguico/palmabici/Synchronizer.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2012 Sergio Garcia Villalonga (yayalose@gmail.com)
+ *
+ * This file is part of PalmaBici.
+ *
+ *    PalmaBici is free software: you can redistribute it and/or modify
+ *    it under the terms of the Affero GNU General Public License version 3
+ *    as published by the Free Software Foundation.
+ *
+ *    PalmaBici is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    Affero GNU General Public License for more details
+ *    (https://www.gnu.org/licenses/agpl-3.0.html).
+ *    
+ */
+
 package com.poguico.palmabici;
 
 import java.io.BufferedReader;
@@ -64,7 +81,7 @@ public class Synchronizer {
 		return instance;
 	}
 	
-	public String getNetworkInfo() {
+	private String getNetworkInfo() {
 		StringBuilder builder = new StringBuilder();
 		HttpClient    client  = new DefaultHttpClient();
 		HttpGet 	  request = new HttpGet(URL);
