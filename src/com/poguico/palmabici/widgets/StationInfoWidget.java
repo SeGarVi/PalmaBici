52be456dbff924e1bb4894ea1f0f3aa58b933a61
Sergio García Villalonga <yayalose@gmail.com>
2014-01-15 23:33:15 +0100

    Changed the condition to show the alarm button in the bubble

--- src/com/poguico/palmabici/widgets/StationInfoWidget.java
+++ src/com/poguico/palmabici/widgets/StationInfoWidget.java
@@ -31,7 +31,6 @@ import org.osmdroid.views.MapView;
 
 import com.poguico.palmabici.R;
 import com.poguico.palmabici.SynchronizableElement;
-import com.poguico.palmabici.network.synchronizer.NetworkSynchronizer;
 import com.poguico.palmabici.synchronizers.LocationSynchronizer;
 import com.poguico.palmabici.util.Formatter;
 import com.poguico.palmabici.util.NetworkInformation;
@@ -45,14 +44,12 @@ public class StationInfoWidget extends DefaultInfoWindow implements Synchronizab
 	private ExtendedOverlayItem  eItem;
 	private Station              station;
 	private ImageButton          alarmButton;
-	private NetworkSynchronizer  networkSync;
 	
 	public StationInfoWidget (MapView mapView, SynchronizableElement parentActivity) {
 		super(R.layout.station_info, mapView);
 		context = parentActivity.getSynchronizableActivity().getApplicationContext();
 		networkInformation = NetworkInformation.getInstance(context);
 		locationSynchronizer = LocationSynchronizer.getInstance(parentActivity);
-		networkSync = NetworkSynchronizer.getInstance(context);
 		alarmButton = (ImageButton)mView.findViewById(R.id.alarmButton);
 	}
 	
@@ -112,43 +109,28 @@ public class StationInfoWidget extends DefaultInfoWindow implements Synchronizab
 					new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT,0));
 		}
 		
-		if (station.getBusy_slots() != 0) {
-			layoutParams = new LinearLayout.LayoutParams(0,0);
-			layoutParams.setMargins(5, 5, 5, 5);
-			alarmButton.setLayoutParams(layoutParams);
-		} else {
+		if (station.getBusy_slots() == 0 &&
+				station.getSlots() != station.getBroken_slots()) {
 			layoutParams = new LinearLayout.LayoutParams(
 					LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
 			layoutParams.setMargins(5, 5, 5, 5);
 			alarmButton.setLayoutParams(layoutParams);
+		} else {
+			layoutParams = new LinearLayout.LayoutParams(0,0);
+			layoutParams.setMargins(5, 5, 5, 5);
+			alarmButton.setLayoutParams(layoutParams);
 		}
 		locationSynchronizer.addSynchronizableElement(this);
-		//networkSync.addSynchronizableElement(this);
 	}
 	
 	@Override
 	public void onClose() {
-		//networkSync.detachSynchronizableElement(this);
 		locationSynchronizer.detachSynchronizableElement(this);
 		super.onClose();
 	}
 
 	@Override
-	public void onSuccessfulNetworkSynchronization() {
-		/*LinearLayout.LayoutParams layoutParams;
-		station = networkInformation.get(eItem.getDescription());
-		
-		if (station.getBusy_slots() != 0) {
-			layoutParams = new LinearLayout.LayoutParams(0,0);
-			layoutParams.setMargins(5, 5, 5, 5);
-			alarmButton.setLayoutParams(layoutParams);
-		} else {
-			layoutParams = new LinearLayout.LayoutParams(
-					LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-			layoutParams.setMargins(5, 5, 5, 5);
-			alarmButton.setLayoutParams(layoutParams);
-		}*/
-	}
+	public void onSuccessfulNetworkSynchronization() {}
 
 	@Override
 	public void onUnsuccessfulNetworkSynchronization() {}
