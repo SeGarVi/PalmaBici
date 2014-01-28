4230a0ca5318e2277a8d023ec8eb592abe86bda9
Sergio García Villalonga <yayalose@gmail.com>
2014-01-28 16:56:54 +0100

    Adapted to the new JSON format

--- src/com/poguico/palmabici/util/Station.java
+++ src/com/poguico/palmabici/util/Station.java
@@ -27,8 +27,8 @@ import android.location.Location;
 import android.location.LocationManager;
 
 public class Station implements Comparable <Station> {
-    private int id, freeSlots, busySlots, brokenSlots, brokenBikes, slots;
-    private String nEstacio, name;
+    private int id, freeSlots, busySlots, slots;
+    private String name;
     private Location location;
     private Float distance;
     private boolean favourite;
@@ -39,42 +39,42 @@ public class Station implements Comparable <Station> {
      * Ugly solution to keep nice names
      * (change it to a properties file)
      */
-    private static final Map<String, String> niceNames;
+    private static final Map<Integer, String> niceNames;
     static {
-        Map<String, String> aMap = new HashMap<String, String>();
-    aMap.put("01", "Parc de ses Veles");
-    aMap.put("06", "Manacor - Manuel Azaña");
-    aMap.put("07", "Aragó - Nuredduna");
-    aMap.put("09", "F. Manuel de los Herreros");
-    aMap.put("13", "Parc de les Estacions");
-    aMap.put("15", "J. Verdaguer - J. Balmes");
-    aMap.put("16", "Parc de sa Riera");
-    aMap.put("17", "Aragó - J. Balmes");
-    aMap.put("18", "Metge Darder");
-    aMap.put("21", "Pl. Alexander Flemimg");
-    aMap.put("24", "Blanquerna - C. de Sallent");
-    aMap.put("25", "Blanquerna - Bartolomé");
-    aMap.put("27", "Pl. París");
-    aMap.put("29", "Institut Balear");
-    aMap.put("31", "Pl. Madrid");
-    aMap.put("34", "Sant Ferran");
-    aMap.put("37", "Av. Argentina");
-    aMap.put("41", "Fàbrica");
-    aMap.put("45", "Jaume III");
-    aMap.put("46", "Pl. Rei Joan Carles I");
-    aMap.put("47", "Pl. Porta de Santa Catalina");
-    aMap.put("49", "Pl. de la Reina");
-    aMap.put("51", "Via Roma");
-    aMap.put("52", "Cecili Metel");
-    aMap.put("55", "Pl. Santa Eulàlia");
+        Map<Integer, String> aMap = new HashMap<Integer, String>();
+    aMap.put(0, "Parc de ses Veles");
+    aMap.put(1, "Manacor - Manuel Azaña");
+    aMap.put(2, "Mateu Enric Lladó");
+    aMap.put(3, "Travessera Ballester");
+    aMap.put(4, "F. Manuel de los Herreros");
+    aMap.put(5, "Aragó - Nuredduna");
+    aMap.put(6, "Aragó - J. Balmes");
+    aMap.put(7, "Pl. Alexander Flemimg");
+    aMap.put(8, "Pl. Madrid");
+    aMap.put(9, "Av. Argentina");
+    aMap.put(10, "Cecili Metel");
+    aMap.put(11, "Institut Balear");
+    aMap.put(12, "Pl. Rei Joan Carles I");
+    aMap.put(13, "Via Roma");
+    aMap.put(14, "Fàbrica");
+    aMap.put(15, "Jaume III");
+    aMap.put(16, "Pl. Porta de Santa Catalina");
+    aMap.put(17, "Pl. de la Reina");
+    aMap.put(18, "Pl. Santa Eulàlia");
+    aMap.put(19, "Parc de les Estacions");
+    aMap.put(20, "J. Verdaguer - J. Balmes");
+    aMap.put(21, "Pl. d'Espanya");
+    aMap.put(22, "Pl. Alexandre Jaume");
+    aMap.put(23, "Parc de sa Riera");
+    aMap.put(24, "Blanquerna - C. de Sallent");
+    aMap.put(25, "Pl. París");
+    aMap.put(26, "Blanquerna - Bartolomé");
+    aMap.put(27, "Metge Darder");
+    aMap.put(28, "Sant Ferran");
     //aMap.put("56", "Pl. del Mercat");
-    aMap.put("59", "Mateu Enric Lladó");
-    aMap.put("60", "Travessera Ballester");
-    aMap.put("63", "Pl. d'Espanya");
-    aMap.put("65", "Pl. Alexandre Jaume");
-    aMap.put("61", "Pl. del Pont");
-    aMap.put("70", "Ctra. de Valldemossa");
-    aMap.put("77", "Son Costa - Son Fortesa");
+    aMap.put(29, "Pl. del Pont");
+    aMap.put(30, "Ctra. de Valldemossa");
+    aMap.put(31, "Son Costa - Son Fortesa");
     niceNames = Collections.unmodifiableMap(aMap);
     }
     
@@ -82,50 +82,46 @@ public class Station implements Comparable <Station> {
      * Ugly solution to keep correct locations
      * (change it to a properties file)
      */
-    private static final Map<String, GeoPoint> correctLocations;
+    private static final Map<Integer, GeoPoint> correctLocations;
     static {
-        Map<String, GeoPoint> aMap = new HashMap<String, GeoPoint>();
-        aMap.put("01", new GeoPoint(39.566129, 2.659499));
-	    aMap.put("06", new GeoPoint(39.571255, 2.665796));
-	    aMap.put("07", new GeoPoint(39.572946, 2.657238));
-	    aMap.put("09", new GeoPoint(39.574468, 2.663871));
-	    aMap.put("13", new GeoPoint(39.575682, 2.654789));
-	    aMap.put("15", new GeoPoint(39.580040, 2.660169));
-	    aMap.put("16", new GeoPoint(39.581745, 2.644123));
-	    aMap.put("17", new GeoPoint(39.578755, 2.662571));
-	    aMap.put("21", new GeoPoint(39.581119, 2.655465));
-	    aMap.put("24", new GeoPoint(39.578097, 2.651191));
-	    aMap.put("25", new GeoPoint(39.580656, 2.649190));
-	    aMap.put("27", new GeoPoint(39.584213, 2.649210));
-	    aMap.put("29", new GeoPoint(39.577311, 2.646439));
-	    aMap.put("31", new GeoPoint(39.577507, 2.640839));
-	    aMap.put("37", new GeoPoint(39.574400, 2.640703));
-	    aMap.put("41", new GeoPoint(39.572710, 2.637534));
-	    aMap.put("45", new GeoPoint(39.572505, 2.643112));
-	    aMap.put("46", new GeoPoint(39.571035, 2.647043));
-	    aMap.put("47", new GeoPoint(39.571295, 2.641806));
-	    aMap.put("49", new GeoPoint(39.568560, 2.646257));
-	    aMap.put("51", new GeoPoint(39.575189, 2.647993));
-	    aMap.put("52", new GeoPoint(39.576453, 2.650509));
-	    aMap.put("55", new GeoPoint(39.569171, 2.650747));
+        Map<Integer, GeoPoint> aMap = new HashMap<Integer, GeoPoint>();
+        aMap.put(0, new GeoPoint(39.566129, 2.659499));
+	    aMap.put(1, new GeoPoint(39.571255, 2.665796));
+	    aMap.put(5, new GeoPoint(39.572946, 2.657238));
+	    aMap.put(4, new GeoPoint(39.574468, 2.663871));
+	    aMap.put(19, new GeoPoint(39.575682, 2.654789));
+	    aMap.put(20, new GeoPoint(39.580040, 2.660169));
+	    aMap.put(23, new GeoPoint(39.581745, 2.644123));
+	    aMap.put(6, new GeoPoint(39.578755, 2.662571));
+	    aMap.put(7, new GeoPoint(39.581119, 2.655465));
+	    aMap.put(24, new GeoPoint(39.578097, 2.651191));
+	    aMap.put(26, new GeoPoint(39.580656, 2.649190));
+	    aMap.put(25, new GeoPoint(39.584213, 2.649210));
+	    aMap.put(11, new GeoPoint(39.577311, 2.646439));
+	    aMap.put(8, new GeoPoint(39.577507, 2.640839));
+	    aMap.put(9, new GeoPoint(39.574400, 2.640703));
+	    aMap.put(14, new GeoPoint(39.572710, 2.637534));
+	    aMap.put(15, new GeoPoint(39.572505, 2.643112));
+	    aMap.put(12, new GeoPoint(39.571035, 2.647043));
+	    aMap.put(16, new GeoPoint(39.571295, 2.641806));
+	    aMap.put(17, new GeoPoint(39.568560, 2.646257));
+	    aMap.put(13, new GeoPoint(39.575189, 2.647993));
+	    aMap.put(10, new GeoPoint(39.576453, 2.650509));
+	    aMap.put(18, new GeoPoint(39.569171, 2.650747));
 	    //aMap.put("56", new GeoPoint(39.571149, 2.648555));
-	    aMap.put("59", new GeoPoint(39.567688, 2.656039));
-	    aMap.put("60", new GeoPoint(39.570273, 2.655827));
-	    aMap.put("63", new GeoPoint(39.575190, 2.654070));
-	    aMap.put("65", new GeoPoint(39.572426, 2.655438));
+	    aMap.put(2, new GeoPoint(39.567688, 2.656039));
+	    aMap.put(3, new GeoPoint(39.570273, 2.655827));
+	    aMap.put(21, new GeoPoint(39.575190, 2.654070));
+	    aMap.put(22, new GeoPoint(39.572426, 2.655438));
 	    correctLocations = Collections.unmodifiableMap(aMap);
     }
     
-    public Station(int id, String nEstacio, String name, double stationLong,
-    		        double stationLat, int freeSlots, int busySlots,
-    		        int brokenSlots, int brokenBikes, boolean favourite,
+    public Station(int id, String name, double stationLong, double stationLat,
+    		        int freeSlots, int busySlots, boolean favourite,
     		        boolean hasAlarm) {
         this.id = id;
-        this.nEstacio = nEstacio;
         this.freeSlots = freeSlots;
         this.busySlots = busySlots;
-        this.brokenSlots = brokenSlots;
-        this.brokenBikes = brokenBikes;
         this.slots = freeSlots + busySlots + brokenSlots;
         this.name = niceNames.get(nEstacio);
         this.favourite = favourite;
@@ -150,14 +146,6 @@ public class Station implements Comparable <Station> {
         this.id = id;
     }
 
-    public String getNEstacio() {
-        return nEstacio;
-    }
-
-    public void setNEstacio(String nEstacio) {
-        this.nEstacio = nEstacio;
-    }
-
     public int getFreeSlots() {
         return freeSlots;
     }
@@ -176,14 +164,6 @@ public class Station implements Comparable <Station> {
 
     public int getSlots() {
         return slots;
-    }
-
-    public int getBrokenSlots() {
-        return brokenSlots;
-    }
-    
-    public int getBrokenBikes() {
-        return brokenBikes;
     }
 
     public String getName() {
